a9478a598b0545848e8f9378ceac0a01f4a00064
danielbelle <henrique.danielb@gmail.com>
2025-04-27 16:52:35 -0300

    vercel config 4

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,12 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/js/app.js"],
+            input: [
+                "resources/admin/scss/style.scss",
+                "resources/css/app.css",
+                "resources/js/app.js",
+            ],
+            refresh: true,
         }),
         tailwindcss(),
     ],
